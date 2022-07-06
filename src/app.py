d8483dffbfe138ad7843350ce18e8e78251aac1d
davidgcorso <davidgcorso@gmail.com>
2022-07-06 15:05:16 +0000

    done

---
+++ src/app.py
@@ -0,0 +1,34 @@
+from flask import Flask
+from flask import jsonify
+from flask import request
+from flask import json
+app = Flask(__name__)
+
+todos = [
+    { "label": "My first task", "done": False },
+    { "label": "My second task", "done": False }
+]
+
+@app.route('/todos', methods=['GET'])
+def hello_world():
+    json_todos = jsonify(todos)
+    return json_todos
+
+@app.route('/todos', methods=['POST'])
+def add_new_todo():
+    request_body = request.data
+    decoded_object = json.loads(request.data)
+    print("Incoming request with the following body", request_body)
+    todos.append(decoded_object)
+    return jsonify(todos)
+    
+@app.route('/todos/<int:position>', methods=['DELETE'])
+def delete_todo(position):
+    print("This is the position to delete: ",position)
+    todos.pop(position)
+    return jsonify(todos)
+
+# Estas dos líneas siempre seben estar al final de tu archivo app.py.
+
+if __name__ == '__main__':
+  app.run(host='0.0.0.0', port=3245, debug=True)
